f392acef4457096628502f21b6f1c1773aeb80e2
Rohit Chopra <rohitchopra32@gmail.com>
2020-09-15 23:04:32 +0530

    code review

--- store/models/order.py
+++ store/models/order.py
@@ -7,14 +7,16 @@ class Order(models.Model):
     is_complete = models.BooleanField(default=False, null=True,blank=False)
     date = models.DateTimeField(auto_now_add=True)
     
+    # again no need for this function use manager for this
     @staticmethod
     def get_order(user_id):
         try:
             order =  Order.objects.get(customer_id=user_id,is_complete=False)
-        except Order.DoesNotExist:
+        except Order.DoesNotExist: # This is not correct way use ObjectDoesNotExists Exception class.
             order = None
         return order
-
+    
+    # you use @property decorator and use OrderItems.objects.aggregate() it is much faster than this.
     def cart_total(self):
         orderitem = self.orderitem_set.all()
         cart= [i.item_total() for i in orderitem]
@@ -47,6 +49,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
     quantity = models.IntegerField(default=0,blank=True,null=True)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    
+    #you can use @property here then you don't need to call it as a function.
     def item_total(self):
         return self.product.price * self.quantity
