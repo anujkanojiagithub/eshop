3015ebfe11ac039ac3a418f4bbe6909fa766b2dd
Rohit Chopra <rohitchopra32@gmail.com>
2020-09-15 23:21:45 +0530

    code review

--- store/templatetags/cart_filters.py
+++ store/templatetags/cart_filters.py
@@ -3,25 +3,21 @@ from store.models.order import Order
 
 register = template.Library()
 
+# check how much lines of code is reduced and are you doing first exists then get?
 @register.filter(name='isInCart')
 def is_exist(prod_id,request):
     user_id = request.session.get('customer')
-    if user_id:
-        user_id = user_id['id']
-        order = Order.get_order(user_id)
-        if order:
-            obj = order.orderitem_set.filter(product_id=prod_id).exists()
-            if obj:
-                obj = order.orderitem_set.get(product_id=prod_id)
-                items= obj.quantity
-            else:
-                items =None
-        else:
-            items = None
-    else :
-        items=None
-    return items
-    
+    if not user_id:
+        return None
+    user_id = user_id['id']
+    order = Order.get_order(user_id)
+    if not order:
+        return None
+    obj = order.orderitem_set.filter(product_id=prod_id).exists()
+    if not obj:
+        return None
+    obj = order.orderitem_set.get(product_id=prod_id)
+    return obj.quantity    
 
 
 @register.filter(name='short_desc')
