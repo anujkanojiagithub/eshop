f4bd3005595dcaacb61f6687fc6d1d925273dd72
Rohit Chopra <rohitchopra32@gmail.com>
2020-09-15 23:37:17 +0530

    code review (#3)

--- store/models/customer.py
+++ store/models/customer.py
@@ -8,6 +8,8 @@ class Customer(models.Model):
 
     def isExist(self):
         return Customer.objects.filter(email=self.email)
+    
+    # again no need for these methods django provides managers for this purpose. If you want to write your custom filters override the manager.
     @staticmethod
     def getCustomerByEmail(email):
         return Customer.objects.get(email=email)
