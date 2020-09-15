cf42813cb4cb02678be79a1914cc388a8badcfa7
Rohit Chopra <rohitchopra32@gmail.com>
2020-09-15 23:37:45 +0530

    code review (#2)

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+# Store Database credentials in ENV or another file
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
